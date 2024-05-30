eeb1f8d6a29a5af5724138634107ee949765147a
Héctor <data_analitics_HLH@protonmail.com>
2024-05-30 11:25:58 +0200

    update functions and test

--- apis/gemma_2b_instruct_api.py
+++ apis/gemma_2b_instruct_api.py
@@ -1,12 +1,24 @@
 import requests
+import json
 
-API_URL = "https://api-inference.huggingface.co/models/google/gemma-2b-it"
+API_URL_GENERATION = "https://api-inference.huggingface.co/models/google/gemma-1.1-2b-it"
 headers = {"Authorization": "Bearer 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"}
 
+
 def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
-	
-output = query({
-	"inputs": "Can you please let us know more details about your ",
-})
+	response = requests.post(API_URL_GENERATION, headers=headers, json=payload)
+	return response.content
+def generate_add_text(input_):
+
+    
+    output = query(
+        {
+        'inputs':f"make a advertisement description of this: {input_} ##",
+        'parameters':{
+            'return_full_text':False,
+            'max_new_tokens' :500
+            }
+        }
+    )
+    output = json.loads(output)
+    return output[0]['generated_text']
--- apis/stable_difuffusion_api.py
+++ apis/stable_difuffusion_api.py
@@ -1,17 +1,20 @@
 import requests
 import io
-import numpy as np
-
-API_URL = "https://api-inference.huggingface.co/models/sd-dreambooth-library/fashion"
+from PIL import Image
+API_URL_DIFUSSION = "https://api-inference.huggingface.co/models/sd-dreambooth-library/fashion"
 headers = {"Authorization": "Bearer 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"}
 
 def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
+	response = requests.post(API_URL_DIFUSSION, headers=headers, json=payload)
 	return response.content
 
 def generate_image_api(inputs):
 	image_bytes = query({
 		"inputs": inputs,
+		'parameters': {
+			'guidance_scale': 9, # How closely follows the prompt
+			'num_inference_steps':100 #steps for the denoising process. the higher, the better. 100 is ok for the demo.
+        }
 	})
 
-	return np.frombuffer(io.BytesIO(image_bytes), dtype=np.float32)
+	return Image.open(io.BytesIO(image_bytes))
